4cf57e0b3da4d3ccfe8de7fb716cdbc954939507
Soham-KT <sohamkothariprof79@gmail.com>
2024-10-26 16:37:29 +0530

    changes in data

--- Flask/app.py
+++ Flask/app.py
@@ -5,15 +5,15 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# @app.route('/data', methods=['GET'])
-# def get_data():
-#     data = {
-#         'Review' : "This DVD will be a disappointment if you get it hoping to see some substantial portion of the acts of the various comics listed on the cover. All you get here are snippets of performance, at best. The rest is just loose-leaf reminiscence about the good old days in Boston, in the early 80's, when a lot of comics were hanging out together and getting their start.It's like a frat house reunion. There's a lot of lame nostalgia. There are quite a few guffaws recalling jokes (practical and otherwise)perpetrated - back then. But you had to have been there to appreciate all the basically good ol' boy camaraderie. If you weren't actually a part of that scene, all this joshing and jostling will fall flat.If you want to actually hear some of these comics' routines - you will have to look elsewhere.",
+@app.route('/data', methods=['GET'])
+def get_data():
+    data = {
+        'Review' : "This DVD will be a disappointment if you get it hoping to see some substantial portion of the acts of the various comics listed on the cover. All you get here are snippets of performance, at best. The rest is just loose-leaf reminiscence about the good old days in Boston, in the early 80's, when a lot of comics were hanging out together and getting their start.It's like a frat house reunion. There's a lot of lame nostalgia. There are quite a few guffaws recalling jokes (practical and otherwise)perpetrated - back then. But you had to have been there to appreciate all the basically good ol' boy camaraderie. If you weren't actually a part of that scene, all this joshing and jostling will fall flat.If you want to actually hear some of these comics' routines - you will have to look elsewhere.",
         
-#         'prediction': 'negative',
-#     }
+        'prediction': 'negative',
+    }
     
-#     return jsonify(data)
+    return jsonify(data)
 
 @app.route("/predict", methods=['POST'])
 def predict():
