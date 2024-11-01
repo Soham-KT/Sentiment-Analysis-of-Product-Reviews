f0eb042a27e4833a0992e679073da250f102c76c
Akanksha Pal <akankshapal1055@gmail.com>
2024-11-01 17:18:56 +0530

    issueresolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
-import ReviewPage from './pages/ReviewPage';
+import ReviewPage from './pages/Reviewpage';
 import './styles/App.css';
 
 function App() {
