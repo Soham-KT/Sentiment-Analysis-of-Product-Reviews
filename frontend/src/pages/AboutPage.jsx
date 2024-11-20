febf615dabcf324adb657c6c47ca4ce3c88f1e28
Akanksha Pal <akankshapal1055@gmail.com>
2024-11-20 20:36:11 +0530

    aboutAgain

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -25,7 +25,6 @@ function AboutPage() {
               <h3>AI/ML Brain</h3>
               <p>
               Hi, I’m Soham Kothari, an aspiring Machine Learning Engineer with over two years of experience in data science and machine learning. Pursuing an integrated B.Tech. and M.Tech. in Information Technology with a CGPA of 8.40, I’ve worked on projects like a Flappy Bird bot using reinforcement learning, a Dockerized voice assistant, advanced gender classification software with YOLOv5, and an LSTM stock prediction model with a 1.9% error margin. I’ve also led the development of my college’s website and a library management system, gaining leadership experience. Additionally, I presented research on gradient descent and multiprocessing performance at an international conference in London.
-
               </p>
             </div>
           </div>
